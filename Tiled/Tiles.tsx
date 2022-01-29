ee4523fab7b4fe17b0a30eaadc6b1436d0119d41
Lex Bailey <danieljabailey@gmail.com>
2022-01-29 03:15:21 +0000

    cursors, selection, automatic route planning

--- Tiled/Tiles.tsx
+++ Tiled/Tiles.tsx
@@ -1,4 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="Tiles" tilewidth="48" tileheight="48" tilecount="100" columns="10">
  <image source="Tiles.png" trans="ffffff" width="480" height="480"/>
+ <tile id="0">
+  <properties>
+   <property name="floor" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="floor" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="floor" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
--- door_jam.py
+++ door_jam.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+import math
 import sys
 import pygame
 import threading
 import time
 from enum import Enum
 from pytmx.util_pygame import load_pygame as load_tmx
+import traceback
+import networkx as nx
 
 target_fps = 30
 
@@ -17,25 +20,56 @@ def surface_geom(w, h, tw, th):
     )
 
 def grid_to_surface(x, y, w, h, tw, th):
+    """ Converts grid coordinates to screen coordinates
+    with 0,0 on the top corner of the topmost tile in the map """
     return (
-        ((tw * (h-1))/2) + (((x-y)*tw)/2)
-        ,((x+y)*th)/2
+        ((tw * (h-1)) + ((x-y)*tw)) / 2
+        ,((x+y)*th) / 2
     )
 
-def surface_to_grid(x, y):
-    pass
+def surface_to_grid(x, y, w, h, tw, th):
+    x_plus_y = (y*2)/th
+    x_minus_y = ((x*2) - (tw * (h-1)))/tw
+    sx = (x_plus_y + x_minus_y) / 2
+    sy = x_plus_y - sx
+    return (
+        math.floor(sx)
+        ,math.floor(sy)
+    )
+
+def add(c1,c2):
+    ((x1,y1),(x2,y2)) = (c1,c2)
+    return (
+        x1+x2
+        ,y1+y2
+    )
+
+def neg(c1):
+    (x,y) = c1
+    return (-x,-y)
+
+def sub(c1,c2):
+    return add(c1, neg(c2))
 
 class Game:
     def __init__(self):
         self.win = pygame.display.set_mode((1000,700))
         self.stop_event = threading.Event()
+        self.can_render = threading.Event()
+        self.can_render.set()
         self.image = pygame.image.load("image.png")
         self.last_time = time.time()
         self.font = pygame.font.SysFont("monospace", 18)
         self.load_map('Tiled/Map1.tmx')
+        self.cursor = None
+        self.selection = None
+        self.path_plan = None
 
     def grid_to_surface(self, x, y):
         return grid_to_surface(x,y,self.w,self.h,self.tw,self.th)
+
+    def surface_to_grid(self, x, y):
+        return surface_to_grid(x,y,self.w,self.h,self.tw,self.th)
 
     def load_map(self, name):
         self.map = load_tmx(name)
@@ -45,20 +79,77 @@ class Game:
         self.th = self.map.tileheight
         self.sh, self.sw = surface_geom(self.w, self.h, self.tw, self.th)
         self.map_surface = pygame.Surface((self.sh,self.sw))
-        for layer in self.map.visible_layers:
+        g = nx.Graph()
+        for i, layer in enumerate(self.map.visible_layers):
             for x, y, img in layer.tiles():
-                self.map_surface.blit(img, self.grid_to_surface(x,y))
+                delta = sub((self.tw/2, self.th), img.get_size())
+                pos = add(self.grid_to_surface(x,y), delta)
+                self.map_surface.blit(img, pos)
+                props = self.map.get_tile_properties(x, y, i)
+                if props and props['floor']:
+                    g.add_node((x,y))
+                    for ox, oy in [(x-1,y),(x,y-1)]:
+                        other = self.map.get_tile_properties(ox, oy, i)
+                        if other and other['floor']:
+                            g.add_edge((x,y), (ox,oy))
+        x,y=self.grid_to_surface(45,45)
+        self.offset = (-x+300,-y+100)
+        self.room = g
 
     def update(self, timediff):
         pass
 
+    def draw_cursor(self, pos, color):
+        cx,cy = pos
+        gx,gy = add(self.offset, self.grid_to_surface(cx,cy))
+        pygame.draw.aalines(self.win, color, True, [
+            (gx,gy)
+            ,(gx+(self.tw/2), gy+(self.th/2))
+            ,(gx,gy+self.th)
+            ,(gx-(self.tw/2), gy+(self.th/2))
+        ])
+
+    def draw_path(self, path, color):
+        if len(path) < 2:
+            return
+        pygame.draw.aalines(self.win, color, False, [
+            add(add(self.offset, self.grid_to_surface(*p)), (0, self.th/2)) for p in path
+        ])
+
     def render(self):
         self.win.fill((0,0,0))
-        x,y=self.grid_to_surface(45,45)
-        self.win.blit(self.map_surface, (-x+300,-y+100))
+        self.win.blit(self.map_surface, self.offset)
+        if self.cursor is not None:
+            self.draw_cursor(self.cursor, (255,0,0))
+        if self.selection is not None:
+            self.draw_cursor(self.selection, (0,255,0))
+        if self.path_plan is not None:
+            self.draw_path(self.path_plan, (255,255,0))
 
+    def to_cursor_pos(self, pos):
+        mouse_pos = sub(pos, self.offset)
+        return self.surface_to_grid(*mouse_pos)
+            
     def event(self, ev):
-        print(f"Unknown event: {ev}")
+        match ev.type:
+            case pygame.MOUSEMOTION:
+                mouse_pos = self.to_cursor_pos(ev.pos)
+                props = self.map.get_tile_properties(*mouse_pos,0)
+                if props and props['floor']:
+                    self.cursor = mouse_pos
+                    if self.selection:
+                        self.path_plan = nx.shortest_path(self.room, self.selection, self.cursor)
+                    else:
+                        self.path_plan = None
+                else:
+                    self.cursor = None
+                    self.path_plan = None
+            case pygame.MOUSEBUTTONDOWN:
+                if ev.button == 1:
+                    self.selection = self.cursor
+                    self.path_plan = None
+            case _:
+                print(f"Unknown event: {ev}")
 
     def fps_counter(self, diff):
         fps = 1/diff
@@ -70,6 +161,8 @@ class Game:
 
     def render_poll(self):
         while not self.stop_event.is_set():
+            self.can_render.wait()
+            self.can_render.clear()
             pygame.fastevent.post(pygame.event.Event(RENDER, {}))
             time.sleep(1/target_fps)
 
@@ -84,12 +177,19 @@ class Game:
                         now = time.time()
                         diff = now - self.last_time
                         self.last_time = now
-                        self.update(diff)
-                        self.render()
+                        try:
+                            self.update(diff)
+                            self.render()
+                        except Exception as e:
+                            traceback.print_exception(e, file=sys.stderr)
                         self.fps_counter(diff)
                         pygame.display.flip()
+                        self.can_render.set()
                     case _:
-                        self.event(ev)
+                        try:
+                            self.event(ev)
+                        except Exception as e:
+                            traceback.print_exception(e, file=sys.stderr)
         except KeyboardInterrupt:
             pass
         self.quit()
