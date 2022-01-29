0c353bb095dbfa115e1da6a1461676e9f7c68bab
Niall Simon <nialljsimon@gmail.com>
2022-01-29 16:33:48 +0000

    Bigger test map

--- Tiled/Tiles.tsx
+++ Tiled/Tiles.tsx
@@ -3,16 +3,37 @@
  <image source="Tiles.png" trans="ffffff" width="480" height="480"/>
  <tile id="0">
   <properties>
+   <property name="can_move_east" type="bool" value="true"/>
+   <property name="can_move_north" type="bool" value="false"/>
+   <property name="can_move_south" type="bool" value="true"/>
+   <property name="can_move_west" type="bool" value="true"/>
    <property name="floor" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="1">
   <properties>
+   <property name="can_move_east" type="bool" value="true"/>
+   <property name="can_move_north" type="bool" value="true"/>
+   <property name="can_move_south" type="bool" value="false"/>
+   <property name="can_move_west" type="bool" value="true"/>
    <property name="floor" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="2">
   <properties>
+   <property name="can_move_east" type="bool" value="false"/>
+   <property name="can_move_north" type="bool" value="true"/>
+   <property name="can_move_south" type="bool" value="true"/>
+   <property name="can_move_west" type="bool" value="true"/>
+   <property name="floor" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="can_move_east" type="bool" value="true"/>
+   <property name="can_move_north" type="bool" value="true"/>
+   <property name="can_move_south" type="bool" value="true"/>
+   <property name="can_move_west" type="bool" value="false"/>
    <property name="floor" type="bool" value="true"/>
   </properties>
  </tile>
