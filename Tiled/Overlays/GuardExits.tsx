954111a3e133bd765f88154efa190fa5dfb84513
Niall Simon <nialljsimon@gmail.com>
2022-01-30 15:59:14 +0000

    split overlay into tiles, add and rename maps, and add character start positions

---
+++ Tiled/Overlays/GuardExits.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="GuardExits" tilewidth="48" tileheight="48" tilecount="32" columns="4">
+ <image source="GuardExits.png" trans="ffffff" width="192" height="384"/>
+</tileset>
