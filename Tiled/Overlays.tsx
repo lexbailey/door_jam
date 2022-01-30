b0d04708d6ede443813d4d8f7f176e5b4fe702be
Niall Simon <nialljsimon@gmail.com>
2022-01-30 01:05:45 +0000

    General cleanup of maps and adding an overlays tileset

---
+++ Tiled/Overlays.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Overlays" tilewidth="48" tileheight="48" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="2">
+  <image width="48" height="48" source="Overlays/GoalArrow.png"/>
+ </tile>
+</tileset>
